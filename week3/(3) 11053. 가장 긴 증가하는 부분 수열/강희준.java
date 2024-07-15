20d252e9045b734446850e3464c5a80d5af19fae
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-15 11:36:37 +0900

    Update 강희준.java
    정우형이 불편해 햐

--- week3/(3) 11053. 가장 긴 증가하는 부분 수열/강희준.java
+++ week3/(3) 11053. 가장 긴 증가하는 부분 수열/강희준.java
@@ -1,37 +1,36 @@
-package algo.bbojjak;
-
 import java.io.*;
 import java.util.*;
 
-public class baek11053 {
+public class Main {
 	static int count;
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 		int[] S = new int[N];
 		int[] T = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			S[i] = Integer.parseInt(st.nextToken());
 		}
 		for (int i = 0; i < N; i++) {
-            T[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (S[i] > S[j] && T[i] < T[j] + 1) {
-                    T[i] = T[j] + 1;
-                }
-            }
-        }
+			T[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (S[i] > S[j] && T[i] < T[j] + 1) {
+					T[i] = T[j] + 1;
+				}
+			}
+		}
 
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            if (max < T[i]) {
-                max = T[i];
-            }
-        }
-        System.out.println(max);
-		
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			if (max < T[i]) {
+				max = T[i];
+			}
+		}
+		System.out.println(max);
+
 	}
 
 }
