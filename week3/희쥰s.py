6a18d0fa155777a0607d5c58e2d072445054cc2c
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-15 11:24:36 +0900

    Update 희쥰s.py

--- week3/희쥰s.py
+++ week3/희쥰s.py
@@ -1,16 +1,1 @@
-import sys
-input = sys.stdin.readline
-
-n,k = map(int,input().split())
-S = []
-A = [0] * (k+1)
-A[0] = 1
-for i in range(n):
-    S.append(int(input()))
-
-
-for a in S:
-    for i in range(a, k + 1):
-        A[i] += A[i-a]
-
-print(A[k])
+// 잘못 올림 나중에 수정할게요~
