912b8830cc0ac616c2857e9bc4893a3a6ccfefda
ChoiJiHooon <159138577+ChoiJiHooon@users.noreply.github.com>
2024-08-05 13:51:01 +0900

    Create 최지훈.java

---
+++ week6/(5) 222-풀링/최지훈.java
@@ -0,0 +1,41 @@
+package week6;
+
+import java.io.*; 
+import java.util.*; 
+
+
+/*
+ * 2 X 2 정사각형의 4가지 수를 order배열에 넣어서 2번째로 큰 수를 구한 다음에, 이를 다시 배열에 넣어서 하나만 남을 때 까지 반복한다. 
+ * */
+public class p5_17289_222풀링 {
+	static int N; 
+	static int[][] pro; 
+	static int[] order = new int[4]; 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		pro = new int[N][N]; 
+		for(int i = 0 ; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				pro[i][j] = Integer.parseInt(st.nextToken()); 
+			}
+		}
+		pooling(N); 
+	}
+	static void pooling(int size) {
+		if(size ==1) {System.out.println(pro[0][0]); return;}
+		else {
+			for(int i = 0; i < N; i+= 2) {
+				for(int j = 0; j < N; j+=2) {
+					order[0] = pro[i][j]; order[1] = pro[i][j+1]; 
+					order[2] = pro[i+1][j]; order[3] = pro[i+1][j+1]; 
+					Arrays.sort(order);
+					pro[i / 2][j / 2] = order[2]; 
+				}
+			}
+			pooling(size / 2); return; 
+		}
+		
+	}
+}
