3d4885f6d019a296aa6606605fff5b71938fd071
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-08-22 13:31:46 +0900

    Create 희준.py

---
+++ week9/(1) 9084. 동전/희준.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+
+    d = [0] * (m + 1)
+    d[0] = 1
+
+
+    for coin in coins:
+        for i in range(coin,m + 1):
+            d[i] += d[i - coin]
+
+    print(d[m])
